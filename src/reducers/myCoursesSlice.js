7d7cba8b548b84d6534f46f92345f594af45d7a6
sungjinsama <a01081440011@gmail.com>
2021-05-12 20:37:55 +0900

    ADD push new course to myCourses

--- src/reducers/myCoursesSlice.js
+++ src/reducers/myCoursesSlice.js
@@ -36,10 +36,31 @@ const myCoursesSlice = createSlice({
         const { myCourses } = action.payload;
 
         state.items = myCourses;
+        state.error = null;
         state.status = "idle";
       }
     },
     [loginUser.rejected]: (state, action) => {
+      if (state.status === "pending") {
+        state.error = action.payload.message;
+        state.status = "idle";
+      }
+    },
+    [saveMyCourse.pending]: (state) => {
+      if (state.status === "idle") {
+        state.status = "pending";
+      }
+    },
+    [saveMyCourse.fulfilled]: (state, action) => {
+      if (state.status === "pending") {
+        const newCourse = action.payload;
+
+        state.items.push(newCourse);
+        state.error = null;
+        state.status = "idle";
+      }
+    },
+    [saveMyCourse.rejected]: (state, action) => {
       if (state.status === "pending") {
         state.error = action.payload.message;
         state.status = "idle";
