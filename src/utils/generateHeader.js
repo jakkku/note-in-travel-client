85d556b96d723d369a723960b8cbdc10c4578f18
sungjinsama <a01081440011@gmail.com>
2021-05-13 13:04:19 +0900

    MODIFY mock token

--- src/utils/generateHeader.js
+++ src/utils/generateHeader.js
@@ -3,7 +3,9 @@ import * as SecureStore from "expo-secure-store";
 async function generateHeader() {
   const defaultHeader = { "Content-Type": "application/json" };
 
-  const token = await SecureStore.getItemAsync("token");
+  // const token = await SecureStore.getItemAsync("token");
+  // TODO: delete this mock
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwOWJlNmU5ZGY4YTE1MzU2YzEzMTM2ZiIsImlhdCI6MTYyMDg3ODQ2NX0.2ooS7p7o9HTu2a9t3AbzzSrqM2JNV0rhTfBOf8bh0k4";
 
   if (!token) {
     return defaultHeader;
