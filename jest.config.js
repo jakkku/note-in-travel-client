e8b96a19ac20c209e1b8d9b64c3332d08f2e256f
sungjinsama <a01081440011@gmail.com>
2021-05-20 00:21:43 +0900

    FIX jest config

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "<rootDir>/testing/jest-setup.js",
   ],
   transform: {
-    "^.+\\.js$": "babel-jest",
+    "^.+\\.jsx?$": "babel-jest",
   },
   transformIgnorePatterns: [
     "node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)",
